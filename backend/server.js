fbfb9d5418dc2a14a46ff388cd3639deafdb3398
deshaanth <deshaanthm.24cse@kongu.edu>
2026-06-17 13:37:16 +0530

    Updated project

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin:"https://atm-appilication.vercel.app",
+  origin: "https://atm-appilication.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
 //app.use(cors());
 app.use(express.json());
